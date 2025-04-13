203907009600f2c760b1573ca7467dd740a01c07
Alyssa Girard <alyssamgirard@gmail.com>
2025-04-12 22:00:42 -0400

    more stuff aaaa

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+// Lists of Clothes
+
+
+// Lower buttons functionality
+const laundryButton = document.getElementById('laundry-button');
+laundryButton.addEventListener('click', function() {
+    window.location.href = 'laundry.html';
+});
+
+const favsButton = document.getElementById('favorites-button');
+favsButton.addEventListener('click', function() {
+    window.location.href = 'favorites.html';
+});
